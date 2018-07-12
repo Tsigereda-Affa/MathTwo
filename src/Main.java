97ab11d4ac537b3edcec1048623ced6e539c1769
tsigereda-affa <affa16.a@gmail.com>
2018-07-12 10:55:21 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main( String[] args ){
+        //Write an application that assigns 2 to a variable named mynumber and 1.7938 to a variable namedmyothernumber, and then prints out the values to the console.
+        int mynumber=2;
+        double namedmyothernumber = 1.7938;
+        System.out.println("My number is:"+ mynumber );
+        System.out.println("Named My Other Number:"+ namedmyothernumber);
+    }
+}
